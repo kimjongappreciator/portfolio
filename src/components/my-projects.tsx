2f8fe82f61341de051420c4ada480c137e18df5f
Sebastian Chacon <nonexistentexistentialist1@gmail.com>
2025-09-09 17:05:02 -0500

    refactor

--- src/components/my-projects.tsx
+++ src/components/my-projects.tsx
@@ -1,10 +1,20 @@
 import { ExplorerBar } from "./explorer-bar";
+import folder from "@/assets/desktop-icons/folder.png"
 
 export function MyProjects() {
   return (
     <>
-      <div className="w-full max-h-[60vh] flex flex-col bg-[#c0c0c0]">
+      <div className="w-full h-[60vh] max-h-[60vh] flex flex-col">
         <ExplorerBar route="C:/Desktop/My Projects"/>
+        <div className="flex overflow-auto">
+          <div className="w-[30%] h-fit border-r border-gray-300 bg-[url(src/assets/projects-folder/explorercorner.jpg)] bg-cover">
+            <img src={folder} alt="folder" className="w-10 h-10 m-4"/>
+            <h2 className="text-lg font-semibold m-4">My Projects</h2>
+          </div>
+          <div className="flex">
+            
+          </div>
+        </div>
       </div>
     </>
   );
