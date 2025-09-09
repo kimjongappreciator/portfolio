576555a2a285049b03389dd2b502260e3103b76e
Sebastian Chacon <nonexistentexistentialist1@gmail.com>
2025-09-09 16:47:50 -0500

    icons

--- src/components/iexplorer-bar.tsx
+++ src/components/iexplorer-bar.tsx
@@ -1,4 +1,6 @@
 import { useT } from "@/store/i18n";
+import back from "@/assets/explorer-bar/back.bmp";
+import forward from "@/assets/explorer-bar/forward.bmp";
 
 export function IExplorerBar({route}: {route?: string}) {
   const t = useT();
@@ -7,14 +9,14 @@ export function IExplorerBar({route}: {route?: string}) {
       <div className="flex items-center gap-1 mb-2">
         
         <button className="explorer-bar-button" title={t?.("back") || "Back"}>
-          ←
+          <img src={back} alt="Back" className="w-fit" />
         </button>
         
         <button
           className="explorer-bar-button"
           title={t?.("forward") || "Forward"}
         >
-          →
+          <img src={forward} alt="Forward" className="w-fit" />
         </button>
         
         <button className="explorer-bar-button" title={t?.("stop") || "Stop"}>
